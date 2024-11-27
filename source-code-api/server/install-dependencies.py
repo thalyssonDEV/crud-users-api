ecc4120940bc8421a8b5dcbf19e88dc0ca059847
thalyssonDEV <167926095+thalyssonDEV@users.noreply.github.com>
2024-11-27 16:38:04 -0300

    Delete source-code-api directory

--- source-code-api/server/install-dependencies.py
+++
@@ -1,9 +0,0 @@
-def install_dependencies():
-    
-    import subprocess
-
-    # Comando para instalar pacotes
-    command_path = "pip install flask httpie requests"
-
-    # Executa o comando
-    subprocess.run(command_path, shell=True, check=True)
